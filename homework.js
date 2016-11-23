386489fb6ad4dea1f51004ce374208ccde2d4d5f
rjshaver <rjshaver@windstream.net>
2016-11-22 23:20:14 -0500

    finished my homework for responsive web design class

---
+++ homework.js
@@ -0,0 +1,14 @@
+			
+	      	var istable = true;
+	      	function clptable(){
+	      		
+	      		if (istable){
+	      			$("#ptable").attr('src', "compsci.jfif"); 
+	      		}else {
+	      			$("#ptable").attr('src', "images.jfif"); 
+	      		}
+	      		istable = !istable;
+
+	      	};
+
+	      
